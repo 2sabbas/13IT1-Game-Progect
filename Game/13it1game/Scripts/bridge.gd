d224a34fdd0d8d05a7ab3302e61d70987d7d7256
2sabbas <2sabbas@hbhs.school.nz>
2026-05-21 11:16:34 +1200

    Finally I can cross the bridge.
    This was done by putting the collision of the water on layer 2 and putting a collision mask on the player for layer 2. then when the bridge area 2D detects the player it disables the mask on the player allowing the player to cross

--- Game/13it1game/Scripts/bridge.gd
+++ Game/13it1game/Scripts/bridge.gd
@@ -11,3 +11,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	label.visible = selected
 	
+
+func _on_selectable_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.set_collision_mask_value(2, false)
+		print("yes")
+
+
+func _on_selectable_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.set_collision_mask_value(2, true)
+		print("no")
