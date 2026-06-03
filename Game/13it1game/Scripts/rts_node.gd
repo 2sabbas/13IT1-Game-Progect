508169a58bebdda52ae7490d26b4ecf6aac3f4ce
2sabbas <2sabbas@hbhs.school.nz>
2026-06-04 11:12:27 +1200

    Finally finished a first working a tilemap copy paste feture (still with bugs)

--- Game/13it1game/Scripts/rts_node.gd
+++ Game/13it1game/Scripts/rts_node.gd
@@ -1,117 +1,127 @@
 extends Node2D
 
-@onready var selection_area: Area2D = $"Selection Area"
-@onready var collision_shape_2d: CollisionShape2D = $"Selection Area/CollisionShape2D"
+@onready var tile_layers: Array = []
+var layers_to_copy = ["Hitbox Elements"]
 
-
-var selectionStartPoint = Vector2.ZERO
+var selection_start_point = Vector2.ZERO
 var clipboard: Array = []
 
+
+func _ready() -> void:
+	var map = get_parent().get_node("Map")
+	for child in map.get_children():
+		if child is TileMapLayer and child.name in layers_to_copy:
+			tile_layers.append(child)
+
+
 func _input(event: InputEvent) -> void:
-	if (selectionStartPoint == Vector2.ZERO && Input.is_action_pressed("left_click")):
-		selectionStartPoint = get_global_mouse_position()
-	elif (selectionStartPoint != Vector2.ZERO && Input.is_action_just_released("left_click")):
-		select_objects()
-		selectionStartPoint = Vector2.ZERO
+	if (selection_start_point == Vector2.ZERO && Input.is_action_pressed("left_click")):
+		selection_start_point = get_global_mouse_position()
+	elif (selection_start_point != Vector2.ZERO && Input.is_action_just_released("left_click")):
+		selection_start_point = Vector2.ZERO
 	
 	# Copy
 	if (Input.is_action_pressed("copy") && Global.numOfCopiesAvalable > 0):
 		copy_selected()
+		Global.numOfCopiesAvalable -= 1
 		
 	# Paste
 	if Input.is_action_pressed("paste"):
 		paste_objects()
 	
 
+
 func _process(delta: float) -> void:
 	queue_redraw()
-	print("clipboard enpty: " + str(!clipboard.is_empty()))
+	#print("clipboard enpty: " + str(clipboard.is_empty()))
+
 
 func copy_selected():
 	clipboard.clear()
-	var selectedObjects = get_tree().get_nodes_in_group("Object").filter(func(o): return o.selected)
+	var mouse_position = get_global_mouse_position()
+	var top_left = Vector2(
+		min(selection_start_point.x, mouse_position.x), 
+		min(selection_start_point.y, mouse_position.y)
+	)
+	var bottom_right = Vector2(
+		max(selection_start_point.x, mouse_position.x), 
+		max(selection_start_point.y, mouse_position.y)
+	)
+	var start_cell = Vector2i.ZERO
+	var end_cell = Vector2i.ZERO
 	
-	if selectedObjects.is_empty():
-		return
-	
-	# find the center of all selected objects so pasting is relative
-	var center = Vector2.ZERO
-	for obj in selectedObjects:
-		center += obj.global_position
-	center /= selectedObjects.size()
-	
-	for obj in selectedObjects:    #this saves a dictonary of the object;s file path and its offset to the center
-		clipboard.append({
-			"scene": obj.scene_file_path,
-			"offset": obj.global_position - center 
-		})
-	Global.numOfCopiesAvalable -= 1
-	print("Copied ", clipboard.size(), " object(s)")
+	for layer in tile_layers:
+		start_cell = layer.local_to_map(layer.to_local(top_left))
+		end_cell = layer.local_to_map(layer.to_local(bottom_right))
+		var layer_clipboard: Array = []
+		
+		for x in range(start_cell.x, end_cell.x + 1):
+			for y in range(start_cell.y, end_cell.y +1):
+				var cell = Vector2i(x, y)
+				var source_id = layer.get_cell_source_id(cell)
+				
+				if source_id == -1:
+					continue  #skip the rest of the loop if the cell is empty
+				
+				layer_clipboard.append({
+					"layer": layer, 
+					"offset": cell - start_cell, 
+					"source_id": source_id, 
+					"atlas_coords": layer.get_cell_atlas_coords(cell),
+					"alternative": layer.get_cell_alternative_tile(cell)
+				})
+		var center_cell = (start_cell + end_cell) / 2
+		for item in layer_clipboard:
+			item["offset"] = item["offset"] - (center_cell - start_cell)
+		clipboard.append_array(layer_clipboard)
+		
+	print("Copied ", clipboard.size(), " tiles")
+
 
 func paste_objects():
-	if clipboard.is_empty():
-		return
+	if clipboard.is_empty(): return
 	
-	var centerOfPaste = get_global_mouse_position()
+	var mouse_position = get_global_mouse_position()
 	
 	for item in clipboard:
-		var packedScene = load(item["scene"]) as PackedScene
-		var newObj = packedScene.instantiate()
-		newObj.position = get_parent().to_local(centerOfPaste + item["offset"])
-		get_parent().get_node("Bridges").add_child(newObj)
+		var layer = item["layer"]
+		var paste_origin = layer.local_to_map(layer.to_local(mouse_position))
+		var target_cell = paste_origin + item["offset"]
 		
-		
-	print("Pasted ", clipboard.size(), " object(s)")
+		layer.set_cell(target_cell, item["source_id"], item["atlas_coords"], item["alternative"])
+	
+	print("Pasted ", clipboard.size(), " tiles")
 	clipboard.clear()	
 
+
 func _draw() -> void:
-	if selectionStartPoint == Vector2.ZERO: return
+	if selection_start_point == Vector2.ZERO: return
 	
-	var mousePosition = get_global_mouse_position()
-	var startX = selectionStartPoint.x
-	var startY = selectionStartPoint.y
-	var endX = mousePosition.x
-	var endY = mousePosition.y
+	var mouse_position = get_global_mouse_position()
+	var start = to_local(selection_start_point)
+	var end = to_local(mouse_position)
 	
 	var lineWidth = 0.5
 	var lineColor = Color.WHITE
+	var fillColor = Color(1, 1, 1, 0.15)
 	
-	draw_line(Vector2(startX, startY), Vector2(endX, startY), lineColor, lineWidth)
-	draw_line(Vector2(startX, startY), Vector2(startX, endY), lineColor, lineWidth)
-	draw_line(Vector2(endX, startY), Vector2(endX, endY), lineColor, lineWidth)
-	draw_line(Vector2(startX, endY), Vector2(endX, endY), lineColor, lineWidth)
+	draw_rect(Rect2(start, end - start), fillColor)
+	draw_line(Vector2(start.x, start.y), Vector2(end.x, start.y), lineColor, lineWidth)
+	draw_line(Vector2(start.x, start.y), Vector2(start.x, end.y), lineColor, lineWidth)
+	draw_line(Vector2(end.x, start.y), Vector2(end.x, end.y), lineColor, lineWidth)
+	draw_line(Vector2(start.x, end.y), Vector2(end.x, end.y), lineColor, lineWidth)
 
-func select_objects():
-	var size = abs(get_global_mouse_position() - selectionStartPoint)
-	var areaPosition = get_rect_start_position()
-	
-	selection_area.global_position = areaPosition
-	collision_shape_2d.position = size / 2
-	collision_shape_2d.shape.size = size
-	
-	await get_tree().create_timer(0.02).timeout
-	
-	var objects = get_tree().get_nodes_in_group("Object")
-	
-	for body in selection_area.get_overlapping_areas():
-		
-		if body.get_parent().is_in_group("Object"):
-			body.get_parent().selected = true
-			objects.erase(body.get_parent())
-	
-	for body in objects:
-		body.selected = false
 
 func get_rect_start_position():
 	var newPosition = Vector2.ZERO
-	var mousePosition = get_global_mouse_position()
+	var mouse_position = get_global_mouse_position()
 	
-	if selectionStartPoint.x < mousePosition.x:
-		newPosition.x = selectionStartPoint.x
-	else: newPosition.x = mousePosition.x
+	if selection_start_point.x < mouse_position.x:
+		newPosition.x = selection_start_point.x
+	else: newPosition.x = mouse_position.x
 	
-	if selectionStartPoint.y < mousePosition.y:
-		newPosition.y = selectionStartPoint.y
-	else: newPosition.y = mousePosition.y
+	if selection_start_point.y < mouse_position.y:
+		newPosition.y = selection_start_point.y
+	else: newPosition.y = mouse_position.y
 	
 	return newPosition
