ce3b21ab527d9c37d7e646f44dacd0a354476d00
2sabbas <2sabbas@hbhs.school.nz>
2026-06-16 09:06:39 +1200

    Merging push

--- Game/13it1game/Scripts/Global.gd
+++ Game/13it1game/Scripts/Global.gd
@@ -1,5 +1,6 @@
 extends Node
 
-var num_of_copies_available = 20
+var num_of_copies_available = 2
+var num_of_undos_available = 1
 var copy_available = false	#set the shortcuts avalavble to what is avalable
 var paste_available = false
--- Game/13it1game/Scripts/hud.gd
+++ Game/13it1game/Scripts/hud.gd
@@ -1,10 +1,13 @@
 extends Control
 
 @onready var copies_available: Label = $"Copies Available"
+@onready var undos_available: Label = $"Undos Available"
 
 func _ready() -> void:
 	copies_available.text = "Copies Available: " + str(Global.num_of_copies_available)
+	undos_available.text = "Undos Available: " + str(Global.num_of_undos_available)
 
 
 func _process(delta: float) -> void:
 	copies_available.text = "Copies Available: " + str(Global.num_of_copies_available)
+	undos_available.text = "Undos Available: " + str(Global.num_of_undos_available)
