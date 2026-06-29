0bc17e4c690da45589489a3424e640fcf136ed1f
2sabbas <2sabbas@hbhs.school.nz>
2026-06-29 20:54:05 +1200

    Made working tutorial instructions. (havent added the instruction text and positions yet)

--- Game/13it1game/Scripts/Global.gd
+++ Game/13it1game/Scripts/Global.gd
@@ -3,8 +3,8 @@ extends Node2D
 enum Game_State {Main_Menu, Controls, Playing_Tutorial, Playing_World, Win}
 var current_game_state: Game_State
 
-var num_of_copies_available = 09999
-var num_of_undos_available = 09999
-var num_of_cuts_available = 09999
+var num_of_copies_available = 0999
+var num_of_undos_available = 0999
+var num_of_cuts_available = 0999
 var paste_available = false
 var selected = false
--- Game/13it1game/Scripts/instruction.gd
+++ Game/13it1game/Scripts/instruction.gd
@@ -2,28 +2,40 @@ extends Area2D
 
 var player_close = false
 
-@onready var label: Label = $Label
-@onready var animation_player: AnimationPlayer = $Label/AnimationPlayer
+@onready var label: Label = $PanelContainer/Label
+@onready var animation_player: AnimationPlayer = $PanelContainer/Label/AnimationPlayer
+
+@export var markers: Array[Marker2D]
 
 var instruction_counter = 0
-var instruction: Array = [
+
+var instructions: Array = [
 	"One", 
 	"Two", 
 ]
 
 
 func _ready() -> void:
-	if player_close == false:
-		animation_player.play("Fade_out_label")
+	global_position = markers[0].global_position
+	label.text = instructions[0]
+	
+	animation_player.play("Fade_out_label")
 	
 
 
 func _process(delta: float) -> void:
-	if player_close && Input.is_action_just_pressed("enter"):
-		if instruction_counter < (instruction.size() - 1):
-			position = Vector2(200,200)      # Call the next_in_group() method or something to go to the next marker 2D to get the next position
-			instruction_counter += 1
-	label.text = instruction[instruction_counter]
+	if player_close and Input.is_action_just_pressed("enter"):
+		next_instruction()
+
+
+func next_instruction():
+	if instruction_counter >= instructions.size() - 1:
+		return
+	
+	instruction_counter += 1
+	
+	global_position = markers[instruction_counter].global_position
+	label.text = instructions[instruction_counter]
 
 
 func _on_body_entered(body: Node2D) -> void:
