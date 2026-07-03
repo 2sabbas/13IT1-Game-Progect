dfe105076de9de9cd9374dde9b5dca08a5d71dc2
2sabbas <2sabbas@hbhs.school.nz>
2026-07-03 22:14:01 +1200

    Added white sifi tilemap for tutorial finish area

--- Game/13it1game/Scripts/pressure_plate.gd
+++ Game/13it1game/Scripts/pressure_plate.gd
@@ -2,8 +2,7 @@ extends Area2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 
-@export var gate_sprite: Sprite2D
-@export var gate_collision_shape: CollisionShape2D
+@export var Gate: StaticBody2D
 
 
 
@@ -18,12 +17,10 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if !self.get_overlapping_bodies().is_empty():
 		sprite.frame = 2
-		gate_sprite.frame = 1
-		gate_collision_shape.set_deferred("disabled", true)
+		Gate.opened = true
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if self.get_overlapping_bodies().is_empty():
 		sprite.frame = 0
-		gate_sprite.frame = 0
-		gate_collision_shape.set_deferred("disabled", false)
+		Gate.opened = false
