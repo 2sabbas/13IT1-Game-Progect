1e1ce160f43548f1474405ab5a5fc1e8d3eee608
2sabbas <2sabbas@hbhs.school.nz>
2026-07-22 07:20:42 +1200

    Made maze puzzle

--- Game/13it1game/Scripts/Global.gd
+++ Game/13it1game/Scripts/Global.gd
@@ -3,15 +3,15 @@ extends Node2D
 enum Game_State {Main_Menu, Controls, Playing_Tutorial, Playing_World, Win}
 var current_game_state: Game_State
 
-var initial_num_of_copies_available = 099
-var initial_num_of_undos_available = 099
-var initial_num_of_cuts_available = 099
+var initial_num_of_copies_available = 0
+var initial_num_of_undos_available = 0
+var initial_num_of_cuts_available = 0
 var initial_paste_available = false
 var initial_selected = false
 
-var num_of_copies_available = 999
-var num_of_undos_available = 999
-var num_of_cuts_available = 999
+var num_of_copies_available = 0
+var num_of_undos_available = 0
+var num_of_cuts_available = 0
 var paste_available = false
 var selected = false
 
